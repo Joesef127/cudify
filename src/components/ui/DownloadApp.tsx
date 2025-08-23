56c20ab3e78be5221bc054360be61cb38a62e29e
Adedayo Adegbola <adegboladayor@gmail.com>
2025-08-23 13:14:08 +0100

    chore: edited button length space between buttons

--- src/components/ui/DownloadApp.tsx
+++ src/components/ui/DownloadApp.tsx
@@ -4,7 +4,7 @@ import playstore from "../../assets/icons/playstore.png";
 
 const DownloadApp = () => {
   return (
-    <div className="grid grid-cols-2 w-full justify-center lg:justify-start gap-3 mt-4">
+    <div className="grid grid-cols-2 w-full max-w-[342px] sm:max-w-[472px] justify-center lg:justify-start gap-1.5 sm:gap-3 mt-4">
       {[
         { icon: apple_logo, isImage: true, children: "App Store" },
         { icon: playstore, isImage: true, children: "Google Play" },
@@ -15,7 +15,7 @@ const DownloadApp = () => {
             icon={btn.icon}
             isImage={btn.isImage}
             children={btn.children}
-            className="py-2 px-3 sm:py-4 sm:px-7 w-full bg-white rounded-full flex gap-2 sm:gap-3 text-[#212337] text-xs sm:text-base font-semibold justify-center items-center"
+            className="py-2 px-3 sm:py-4 sm:px-7 w-full max-w-[163px] sm:max-w-[256px] bg-white rounded-full flex gap-2 sm:gap-3 text-[#212337] text-xs sm:text-base font-semibold justify-center items-center"
           />
         );
       })}
