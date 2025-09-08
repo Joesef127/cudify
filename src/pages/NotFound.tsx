2047cae7809901bc6753c31149e772ea43a900af
Adedayo Adegbola <adegboladayor@gmail.com>
2025-09-08 14:36:03 +0100

    chore: edited notfound page

--- src/pages/NotFound.tsx
+++ src/pages/NotFound.tsx
@@ -2,7 +2,7 @@ import { Link } from "react-router-dom";
 
 export default function NotFound() {
   return (
-    <>
+    <div className="w-full">
       <main className="grid min-h-full place-items-center bg-gray-900 px-6 py-24 sm:py-32 lg:px-8">
         <div className="text-center">
           <p className="text-base font-semibold text-indigo-400">404</p>
@@ -25,6 +25,6 @@ export default function NotFound() {
           </div>
         </div>
       </main>
-    </>
+    </div>
   )
 }
